2a879ffcc334ff18f11a5a57951b96bc34c01f02
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-03-11 02:24:01 +0600

    Initial Commit

---
+++ lib/back_end/auth.dart
@@ -0,0 +1,38 @@
+import 'dart:developer';  // Importing for log function
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthSetup {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Create Account =============================================================
+  Future<User?> createUserWithEmailAndPassword(String email, String password) async {
+    try {
+      final UserCredential cred = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return cred.user;
+    } catch (e) {
+      if (e is FirebaseAuthException) {
+        log('Firebase Error: ${e.message}');
+      } else {
+        log('Unknown Error: $e');
+      }
+      return null;
+    }
+  }
+
+  //Login Account =============================================================
+  Future<User?> loginUserWithEmailAndPassword(String email, String password) async {
+    try {
+      final UserCredential cred = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return cred.user;
+    }catch (e){
+      if(e is FirebaseAuthException) {
+        log("Firebase Error: ${e.message}");
+      } else {
+        log("Unknown Error: $e");
+      }
+      return null;
+    }
+  }
+}
