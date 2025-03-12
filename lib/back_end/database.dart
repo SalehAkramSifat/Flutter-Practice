047f4757c176830764cc8617a25f85cf2f39ca0e
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-03-12 15:43:15 +0600

    Initial Commit

---
+++ lib/back_end/database.dart
@@ -0,0 +1,47 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  // Constructor
+  DatabaseHelper._init();
+
+  // Database getter (Singleton)
+  Future<Database?> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database;
+  }
+
+  // Initialize the database
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, "notes_database.db");
+    print('Database Path: $path');
+
+    return await openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute('''CREATE TABLE notes(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)''');
+    });
+  }
+
+  // Insert Note function
+  Future<void> insertNote(String content) async {
+    try {
+      final db = await database; // Get the database instance
+      if (db != null) {
+        await db.insert(
+          'notes',
+          {'content': content},
+          conflictAlgorithm: ConflictAlgorithm.replace,
+        );
+        print('Inserted Note: $content');
+      } else {
+        print("Error: Database is null");
+      }
+    } catch (e) {
+      print('Error inserting note: $e'); // Catch any errors during insert
+    }
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("SQLite Database"),
+      centerTitle: true,backgroundColor: Colors.blue,),
+
+      body: Center(child: Padding(padding: EdgeInsets.all(15),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(padding: EdgeInsets.all(15)),
+            TextField(decoration: InputDecoration(labelText: "Enter Your Text",
+            border: OutlineInputBorder()),
+              minLines: 1, maxLines: 3,),
+
+            Align(alignment: Alignment.centerLeft,
+                child: TextButton(onPressed: (){}, child: Text('Save'),
+                    style: TextButton.styleFrom(backgroundColor: Colors.black26,),)),
+
+            SizedBox(height: 20),
+            Expanded(child: ListView.builder(itemBuilder: ))
+
+
+      ],),
+
+
+
+
+
+
+
+
+
+
+    )));
+  }
+
+}
