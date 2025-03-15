8ad72bc03635e3f6399686eec4dbe3527c676a10
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-03-15 23:43:31 +0600

    Initial Commit

--- lib/back_end/database.dart
+++ lib/back_end/database.dart
@@ -1,5 +1,3 @@
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
@@ -7,43 +5,8 @@ class DatabaseHelper {
   DatabaseHelper._init();
 
   Future<Database?> get database async {
-    if (_database != null) return _database!;
+    if(_database != null) return _database;
     _database = await _initDB();
     return _database;
-  }
-
-  Future<Database> _initDB() async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, "notes_database.db");
-    print('Database Path : $path');
-
-    return await openDatabase(path, version: 1, onCreate: (db, version) async {
-      await db. execute('''CREATE TABLE notes(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)'''
-      );
-    });
-  }
-
-  //Insert Note ================================================================
-  Future<List<Map<String, dynamic>>?> getNotes() async {
-    final db = await database;
-    if(db == null) {
-      print('Error: Database is null');
-      return null;
-    }
-    var result = await db.query('notes');
-    print('Database Notes: $result');
-    return result;
-  }
-
-  //Delete Note ================================================================
-  Future<void> deleteNotes (int id) async {
-    final db = await database;
-    if (db == null) {
-      print('Error: Database is null');
-      return;
-    }
-
-    await db.delete('notes', where: 'id = ?', whereArgs: [id]);
-    print('Deleted Note with ID: $id');
   }
 }
