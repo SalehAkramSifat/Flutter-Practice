3d23267393a992e229b6d74d3e7bc360b8e2b7fc
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-03-15 23:50:12 +0600

    Initial Commit

--- lib/back_end/database.dart
+++ lib/back_end/database.dart
@@ -1,3 +1,6 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
@@ -8,5 +11,12 @@ class DatabaseHelper {
     if(_database != null) return _database;
     _database = await _initDB();
     return _database;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, "notes_database.db");
+    print('Database Path : $path');
+
   }
 }
